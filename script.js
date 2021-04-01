7386d0218e22eefec0efc107141a8708fcdd9137
Sarthak Sharma <sarthaksharma_ma@srmuniv.edu.in>
2021-04-01 10:12:02 +0530

    one page done

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var button_translate = document.querySelector(".btn-translate")
-var txt_area = document.querySelector(".txt-area")
-var output = document.querySelector(".output")
+// var button_translate = document.querySelector(".btn-translate")
+// var txt_area = document.querySelector(".txt-area")
+// var output = document.querySelector(".output")
 
-var serverURL= "https://api.funtranslations.com/translate/minion.json"
-function getTranslationURL(text){
-    return serverURL+"?"+"text="+text
-}
-function errorHandler(error){
-    console.log("error occured", error)
-    alert("something wrong with server! try again after some time.")
-}
-function clickEventHandler(){
-    var inputText = txt_area.value;
-    fetch(getTranslationURL(inputText)).then(response => response.json()).then(json => {
-        var translatedText = json.contents.translated;
-        output.innerText = translatedText;
-    })  .catch(errorHandler)
-}
-button_translate.addEventListener("click", clickEventHandler)
+// var serverURL= "https://api.funtranslations.com/translate/minion.json"
+// function getTranslationURL(text){
+//     return serverURL+"?"+"text="+text
+// }
+// function errorHandler(error){
+//     console.log("error occured", error)
+//     alert("something wrong with server! try again after some time.")
+// }
+// function clickEventHandler(){
+//     var inputText = txt_area.value;
+//     fetch(getTranslationURL(inputText)).then(response => response.json()).then(json => {
+//         var translatedText = json.contents.translated;
+//         output.innerText = translatedText;
+//     })  .catch(errorHandler)
+// }
+// button_translate.addEventListener("click", clickEventHandler)
---
+++ scriptt.js
@@ -0,0 +1,32 @@
+
+
+var b= document.querySelector(".btn-translate");
+var i=document.querySelector(".txt-area");
+var o=document.querySelector(".output");
+
+var url="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return url+"?"+"text="+text;
+}
+
+
+function errorHandler(error){
+    console.log("error occured",error)
+}
+
+
+
+function clickHandler(){
+ var inputText=i.value; 
+
+    fetch(getTranslationURL(inputText))
+      .then(response => response.json())
+      .then(json => {
+          var translatedText=json.contents.translated;
+          o.innerText=translatedText;
+      })
+      .catch(errorHandler)
+};
+
+b.addEventListener("click", clickHandler)
